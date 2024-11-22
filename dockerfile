8c18d992416f56aa0f447ad440f1f22d8765363e
Leon Ng <leon.ng@r2c2.ai>
2024-11-23 05:18:58 +0800

    dockerfile, update newsletter

---
+++ dockerfile
@@ -0,0 +1,62 @@
+# referenced from next.js example dockerfile
+FROM node:20.18.0-alpine AS base
+
+# install dependencies
+FROM base AS deps
+
+WORKDIR /app
+
+COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* .npmrc* ./
+RUN \
+  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+  elif [ -f package-lock.json ]; then npm ci; \
+  elif [ -f pnpm-lock.yaml ]; then corepack enable pnpm && pnpm i --frozen-lockfile; \
+  else echo "Lockfile not found." && exit 1; \
+  fi
+
+
+# Rebuild the source code only when needed
+FROM base AS builder
+
+WORKDIR /app
+
+RUN npm config set strict-ssl false
+
+COPY --from=deps /app/node_modules ./node_modules
+
+COPY . .
+
+ENV NEXT_TELEMTRY_DISABLED=1
+
+RUN \
+  if [ -f yarn.lock ]; then yarn run build; \
+  elif [ -f package-lock.json ]; then npm run build; \
+  elif [ -f pnpm-lock.yaml ]; then corepack enable pnpm && pnpm run build; \
+  else echo "Lockfile not found." && exit 1; \
+  fi
+
+# Production image, copy all the files and run next
+FROM base AS runner
+WORKDIR /app
+
+ENV NODE_ENV=production
+ENV NEXT_TELEMTRY_DISABLED=1
+
+RUN addgroup --system --gid 1001 nodejs
+RUN adduser --system --uid 1001 nextjs
+
+RUN mkdir .next
+RUN chown nextjs:nodejs .next
+
+
+COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
+COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+COPY --chown=nextjs:nodejs ./public ./public
+
+USER nextjs
+ENV PORT=3000
+EXPOSE 3000
+
+ENV HOSTNAME="0.0.0.0"
+COPY ./entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -9,119 +9,28 @@ import {
 
 export default function AboutPage() {
   return (
-    <div>
-      <section className="flex flex-row flex-wrap place-content-center bg-pciWhite py-24">
-        <Card className="w-full max-w-[96rem] max-h-[25rem] flex-row my-8 mx-8">
-          <CardHeader
-            shadow={false}
-            floated={false}
-            className="m-0 w-1/5 shrink-0 rounded-r-none"
-          >
-            <img
-              src="/images/julianchan.png"
-              alt="card-image"
-              className="h-full w-full object-cover"
-            />
-          </CardHeader>
-          <CardBody>
-            <Typography variant="h6" color="gray" className="mb-4 ">
-              Principal and Lead Data Scientist, Bates White Economic Consulting
-            </Typography>
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              Julian TszKin Chan, PhD
-            </Typography>
-            <Typography color="gray" className="mb-8 font-normal">
-              Julian TszKin Chan is a Managing Economist and Lead Data Scientist
-              in the Finance practice at Bates White Economic Consulting. He has
-              significant experience conducting quantitative and qualitative
-              analyses in support of expert economic testimony, including
-              econometric modeling, Monte Carlo simulation, damages estimation,
-              statistical sampling, and processing big data. Dr. Chan is an
-              experienced researcher with interests in applying econometric, big
-              data, machine learning and natural-language processing methods to
-              analyze economic phenomena, including questions related to social
-              networks, social media, and cryptocurrencies.{" "}
-            </Typography>
-            <a href="#" className="inline-block">
-              <Button variant="text" className="flex items-center gap-2">
-                Learn More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  className="h-4 w-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                  />
-                </svg>
-              </Button>
-            </a>
-          </CardBody>
-        </Card>
-
-        <Card className="w-full max-w-[96rem] max-h-[25rem] flex-row my-5 mx-8">
-          <CardHeader
-            shadow={false}
-            floated={false}
-            className="m-0 w-1/5 shrink-0 rounded-r-none"
-          >
-            <img
-              src="/images/weifengzhong.jpg"
-              alt="card-image"
-              className="h-full w-full object-cover"
-            />
-          </CardHeader>
-          <CardBody>
-            <Typography variant="h6" color="gray" className="mb-4 ">
-              Senior Advisor, America First Policy Institute
-            </Typography>
-            <Typography variant="h6" color="gray" className="mb-4 ">
-              Affiliated Scholar, Mercatus Center at George Mason University
-            </Typography>
-            <Typography variant="h4" color="blue-gray" className="mb-2">
-              Weifeng Zhong, PhD
-            </Typography>
-            <Typography color="gray" className="mb-8 font-normal">
-              Weifeng Zhong serves as a Senior Advisor for the Office for Fiscal
-              and Regulatory Analysis at the America First Policy Institute and
-              an Affiliated Scholar at the Mercatus Center at George Mason
-              University. He has over a decade of experience applying data
-              analytics and artificial intelligence to policy questions. Zhong
-              received his Ph.D. in managerial economics and strategy at
-              Northwestern University and, before joining AFPI, was a scholar at
-              the American Enterprise Institute and Mercatus. His work has been
-              featured in media outlets like The Wall Street Journal, Financial
-              Times, Bloomberg, NPR, Foreign Affairs, and Lawfare. He’s also a
-              frequent interview guest on BBC, C-SPAN, and Fox Business, among
-              others.
-            </Typography>
-            <a href="#" className="inline-block">
-              <Button variant="text" className="flex items-center gap-2">
-                Learn More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  className="h-4 w-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                  />
-                </svg>
-              </Button>
-            </a>
-          </CardBody>
-        </Card>
-      </section>
-    </div>
+    <section className="w-full flex flex-col place-items-center py-16 text-black dark:text-white bg-white dark:bg-pciDark">
+      <Typography className={"max-w-7xl"} variant="h1">
+        {"Newsletter".toUpperCase()}
+      </Typography>
+      <Typography className="max-w-7xl my-8" variant="lead">
+        {
+          "The Policy Change Index newsletter delivers routine updates on the series of PCI projects to your inbox through Substack.\n"
+        }
+        <br />
+        If you are not in the loop yet,{" "}
+        <a href="https://policychangeindex.substack.com/">sign up here</a>.
+        <br />
+        {
+          "Below is the latest issue of the newsletter. Check out our Substack page for the archive."
+        }
+      </Typography>
+      <div className="w-full flex place-content-center">
+        <embed
+          className="block w-[60rem] h-[80rem]"
+          src={"/newsletter/latest.pdf"}
+        ></embed>
+      </div>
+    </section>
   );
 }
