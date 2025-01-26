37a80600b73235b727d277890de0f64b9e33b738
Leon Ng <minister_852@yahoo.com>
2025-01-26 10:46:24 +0000

    fix staging version display

--- scripts/prod_env.sh
+++ scripts/prod_env.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cp .env.repo .env
-echo "NODE_PUBLIC_STAGING=false" >> .env
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SRC=${DIR}/../.env.repo
+DEST=${DIR}/../.env
+cp $SRC $DEST
+echo $'\nNEXT_PUBLIC_STAGING=false' >> $DEST
+cat $DEST
